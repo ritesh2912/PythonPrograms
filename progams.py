841214aa7177fb51483885ea64dbf0bfef6293ea
ritesh2912 <94890713+ritesh2912@users.noreply.github.com>
2022-07-06 23:36:37 +0530

    Update progams.py

--- progams.py
+++ progams.py
@@ -2,7 +2,7 @@
 # "Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, 
 # How I wonder what you are"
 
-#print("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are!")
+print("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are!")
 
 #2. Write a Python program to get the Python version you are using
 import sys
